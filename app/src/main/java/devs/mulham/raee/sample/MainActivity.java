8a4b0f63d6bde5fa927507e8ae1dd64d385b813a
atkawa7 <atkawa7@yahoo.com>
2019-11-26 05:23:05 +0200

    change to android x horizontal calendar

--- app/src/main/java/devs/mulham/raee/sample/MainActivity.java
+++ app/src/main/java/devs/mulham/raee/sample/MainActivity.java
@@ -2,9 +2,9 @@ package devs.mulham.raee.sample;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
